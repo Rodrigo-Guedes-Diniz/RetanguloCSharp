36f0f2cc5231b46ca7af950b22e94c4b933a8828
Rodrigo-Guedes-Diniz <rg510760@gmail.com>
2024-11-24 20:14:36 -0300

    subindo projeto

---
+++ ExercicioRetangulo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioRetangulo {
+    internal class Program {
+
+        static void Main(string[] args) {
+
+            Retangulo retangulo = new Retangulo();
+
+            Console.WriteLine("entre a largura e a altura do retângulo para calcular a Area, o Perímetro e a Diagonal");
+            Console.Write("Altura: ");
+            retangulo.altura = double.Parse(Console.ReadLine());
+            Console.Write("Largura: ");
+            retangulo.largura = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Area = " + retangulo.Area());
+            Console.WriteLine("Perimetro = " + retangulo.Perimetro());
+            Console.WriteLine("Diagonal = " + retangulo.Diagonal());
+
+
+
+        }
+    }
+}
